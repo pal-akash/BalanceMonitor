2a3b84699f68ae97dae0f6a7adbdf164356b8f2e
Akash Pal <108969268+pal-akash@users.noreply.github.com>
2024-07-03 18:10:49 +0530

    [feat] delete account

--- script.js
+++ script.js
@@ -125,7 +125,6 @@ btnLogin.addEventListener('click', function(e){
   e.preventDefault();
 
   currentAccount = accounts.find(acc => acc.username === inputLoginUsername.value);
-  console.log(currentAccount);
 
   if(currentAccount?.pin === Number(inputLoginPin.value)){
     // console.log('login');
@@ -158,4 +157,23 @@ btnTransfer.addEventListener('click', function(e){
   }
 
   inputTransferAmount.value = inputTransferTo.value = '';
+});
+
+btnClose.addEventListener('click', function(e){
+  e.preventDefault();
+
+  if(inputCloseUsername.value === currentAccount.username && Number(inputClosePin.value) === currentAccount.pin){
+
+    const index = accounts.findIndex(acc => acc.username === currentAccount.username);
+
+    //delete account
+    accounts.splice(index, 1);
+
+    // logout or hide ui
+    containerApp.style.opacity = 0;
+
+  }
+
+  inputCloseUsername.value = inputClosePin.value = '';
+
 });
